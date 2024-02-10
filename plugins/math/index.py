0fc259d5886aa82a93b9da15e8fe0edf0e1e347a
JamesFrench <119378901+JamesMinoucha@users.noreply.github.com>
2024-02-10 18:48:48 +0100

    Add files via upload

--- plugins/math/index.py
+++ plugins/math/index.py
@@ -1,1 +1,5 @@
+import system.plugin as lb
 
+def addition(*args):
+    print(int(args[0])+int(args[1]))
+    lb.summonError('This is a example library, yeah there is nothin interesting lol', pluginName='Math (wip)')
