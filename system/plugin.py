2c42cdcc8d534e9a0e33418d5e02fdc63e3429a1
JamesFrench <119378901+JamesMinoucha@users.noreply.github.com>
2024-06-24 13:20:43 +0200

    Delete system/plugin.py

--- system/plugin.py
+++
@@ -1,7 +0,0 @@
-from rich import print
-
-def summonError(error='An error has occurred',pluginName=''):
-    if error == 'An error has occurred' or error == '':
-        print(f'[red]An error has occurred{f' with [bold]{pluginName}[/bold]' if pluginName != '' else ''}[/red]')
-    else:
-        print(f'[red]{f'[bold]{pluginName}[/bold]: ' if pluginName != '' else ''}{error}[/red]')
